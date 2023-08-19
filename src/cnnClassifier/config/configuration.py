1c760aacf416c9fc01e8f7888d3750f1cba285db
Tarun Kuhar <tarunkuhar@Taruns-MacBook-Air.local>
2023-08-19 14:34:10 +0530

    training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,8 +1,12 @@
-from cnnClassifier import *
-import os
-from cnnClassifier.utils.common import read_yaml,create_directories
 from cnnClassifier.constants import *
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,PrepareCallbacksConfig)
+import os
+from pathlib import Path
+from cnnClassifier.utils.common import read_yaml, create_directories
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig,
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
+
 
 
 class ConfigurationManager:
@@ -15,8 +19,9 @@ class ConfigurationManager:
         self.params = read_yaml(params_filepath)
 
         create_directories([self.config.artifacts_root])
-        
 
+
+    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -28,8 +33,11 @@ class ConfigurationManager:
             local_data_file=config.local_data_file,
             unzip_dir=config.unzip_dir 
         )
-        
+
         return data_ingestion_config
+    
+
+
     
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
@@ -48,7 +56,7 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
-      
+    
 
 
     def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
@@ -66,4 +74,28 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
     
